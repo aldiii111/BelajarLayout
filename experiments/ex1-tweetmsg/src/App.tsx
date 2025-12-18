8aa8a291e7446aaf1439784fd3483e23c6906d7a
aldiii <aldinissa093@gmail.com>
2025-12-18 23:15:52 +0700

    just tryyy

--- experiments/ex1-tweetmsg/src/App.tsx
+++ experiments/ex1-tweetmsg/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="container-main">
+        <div className="header-msg">
+          <div className="profile">
+            <img src="/me.jpg" alt="profil" />
+            <div className="name">
+              <p>Aldi Kurniawan</p>
+              <p>aldiii.mutu@gmail.com</p>
+            </div>
+          </div>
+          <div className="logos-twitt">
+            <img src="/twitter.png" alt="twiter logo" />
+          </div>
+        </div>
+        <p className="text-msg">
+          Why do they call it 'debugging' when it feels more like 'wildly guessing and hoping for best'?<a href="#">#programming</a><a href="#">#coding</a><a href="#">#debugging</a>
         </p>
+        <div className="bottom-msg">
+          <div className="like-msg">
+            <button className="heart-button" onClick={() => setCount(count + 1)}>
+              <img src="/heart.png" alt="like" />
+            </button>
+            <p className="the-like">{count}</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
